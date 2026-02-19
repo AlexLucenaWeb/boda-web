b5babce68ee0f89a4b44dd37e409e144db24938f
alejandrolucena-dmi <alejandro@digitalmarketinginstitute.com>
2026-02-19 21:45:22 +0100

    test commit

---
+++ src/components/HeroImage.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export default function HeroImage() {
+
+  return (
+    <section className="relative max-w-full flex justify-center h-screen overflow-hidden">
+        Alex
+
+    </section>
+  );
+}
